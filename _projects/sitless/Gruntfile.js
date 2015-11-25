9bfa1ad655034fe3928159631a28b57f66503caa
Geoffrey Liu <geoffliu@uw.edu>
2015-11-24 23:19:52 -0800

    grunt serve - a new way to deploy the website

--- _projects/sitless/Gruntfile.js
+++ _projects/sitless/Gruntfile.js
@@ -19,14 +19,27 @@ module.exports = function(grunt) {
                 },
                 sourcemap: 'none'
             }
+        },
+        connect: {
+            server: {
+                options: {
+                    hostname: 'localhost',
+                    keepalive: true,
+                    base: './',
+                    livereload: true
+                }
+            }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
     // Default task(s).
     grunt.registerTask('default', ['uglify', 'sass:dist']);
+
+    grunt.registerTask('serve', ['uglify', 'sass:dist', 'connect']);
 
 };
