55ed25f188c75c40db95a324d69f56b4f8eba84f
Geoffrey Liu <geoffliu@uw.edu>
2015-12-12 02:41:25 -0800

    Header mostly done, minor enhancements and trials

--- _projects/sitless/assets/scripts.js
+++ _projects/sitless/assets/scripts.js
@@ -1,21 +1,28 @@
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*=#]').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
+      if (this.hash) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 51
+          }, 500);
+        }
+      } else {
         $('html,body').animate({
-          scrollTop: target.offset().top
+          scrollTop: 0
         }, 500);
-        return false;
       }
+      return false;
     }
   });
+  
+  if ( ($(window).height() + 200) < $(document).height() ) {
+      $('#top-link-block').removeClass('hidden').affix({
+          // how far to scroll down before link "slides" into view
+          offset: {top: 200}
+      });
+  }
 });
 
-if ( ($(window).height() + 100) < $(document).height() ) {
-    $('#top-link-block').removeClass('hidden').affix({
-        // how far to scroll down before link "slides" into view
-        offset: {top:100}
-    });
-}
